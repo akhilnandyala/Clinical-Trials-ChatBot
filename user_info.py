e2e6bd267f6792daa2387472747db42a7028805f
akhil-bowhead <65965856+akhil-bowhead@users.noreply.github.com>
2020-10-13 16:20:55 -0400

    user info modif

--- user_info.py
+++ user_info.py
@@ -4,11 +4,18 @@ import streamlit as st
 
 def user_info():
     question = 'let me get some basic info before I can help you with your query'
-    st.text_area("Bot:", value=question, height=200, max_chars=None, key=None)
+    display_output(question)
     question = 'Can you let me know your location ?'
-    st.text_area("Bot:", value=question, height=200, max_chars=None, key=None)
-    user_location = st.text_input("You: ")
-    st.text_area("Bot:", value='Thank you', height=200, max_chars=None, key=None)
+    display_output(question)
+    user_location = take_input()
+    question = 'Thank you'
+    display_output(question)
 
+def display_output(question):
+    st.text_area("Bot:", value=question, height=200, max_chars=None, key=None)
+
+def take_input():
+    answer = st.text_input("You: ")
+    return answer
 
 
