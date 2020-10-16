d2b8f76a74c9b9d0c504930b349f6d77b4c6bbe2
akhil-bowhead <65965856+akhil-bowhead@users.noreply.github.com>
2020-10-16 16:45:37 -0400

    new addition50

--- sample.py
+++ sample.py
@@ -1,3 +1,15 @@
-import pyaudio
-
-print(pyaudio.__version__)
+# import pyaudio
+# import speech_recognition as sr
+#
+# print(pyaudio.__version__)
+# print(sr.__version__)
+#
+# r = sr.Recognizer()
+#
+# mic = sr.Microphone()
+#
+# print(sr.Microphone.list_microphone_names())
+#
+# with mic as source:
+#     audio = r.listen(source)
+#     print(r.recognize_google(audio))
