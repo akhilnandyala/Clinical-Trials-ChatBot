32748a8769699d407cb0700ae4d50d1d88cbefe8
akhil-bowhead <65965856+akhil-bowhead@users.noreply.github.com>
2020-10-23 14:21:29 -0400

    Modification and audio code inclusion

--- app.py
+++ app.py
@@ -8,6 +8,10 @@ from PIL import Image
 import streamlit as st
 import api as api
 import re
+import speech_recognition as sr
+from gtts import gTTS
+import os
+from playsound import playsound
 
 
 # paths
@@ -30,6 +34,7 @@ condition_df = pd.read_csv(Path.joinpath(Path.cwd(), 'medical_condition.csv'))
 world_cities_df = pd.read_csv(Path.joinpath(Path.cwd(), 'world-cities_csv.csv'))
 
 original_input_text = ''
+voice_input_check = 0
 
 def get_pred(model, encoded_input):
     pred = np.argmax(model.predict(encoded_input))
@@ -65,13 +70,12 @@ def botResponse(user_input, user_name, user_location, user_age, user_gender='All
     a = df_input.iloc[0]['questions']
     if pred == 0:
         if not a:
-            default_response = 'Hi {}, I am Bowhead Bot, I can help you get to know more about Bowhead Health and the services we provide. I can also help you find information about medical trials'.format(user_name)
+            default_response = 'Hi {}, I am Bowhead Bot, I can help you get to know more about Bowhead Health and the services we provide. I can also help you find information about medical trials.'.format(user_name)
             response = default_response
         else:
             response = get_response(df2, pred)
             response = bot_response(response)
     elif pred == 1:
-        input_string = df_input.iloc[0]['questions']
         for i, r in condition_df.iterrows():
             med_condition_word_list = r['med_condition'].split()
             med_condition_word_combo_list = list(map(' '.join, zip(med_condition_word_list[:-1], med_condition_word_list[1:])))
@@ -108,47 +112,58 @@ def botResponse(user_input, user_name, user_location, user_age, user_gender='All
 
 
 def get_text():
+    # global voice_input_check
+    # if st.button('Voice Input'):
+    #     voice_input_check = 1
+    #     r = sr.Recognizer()
+    #     mic = sr.Microphone()
+    #     with mic as source:
+    #         audio = r.listen(source)
+    #         try:
+    #             input_text = r.recognize_google(audio, language='en-US')
+    #         except sr.UnknownValueError:
+    #             input_text = "Sorry could not understand, Please try again"
+    #     st.write(input_text)
+    # else:
+    #     input_text = st.text_input("You: ", key=3)
+
+# If above audio code is enabled, comment out the below line of code, else uncomment
     input_text = st.text_input("You: ", key=3)
+
     global original_input_text
     original_input_text = input_text
     df_input = pd.DataFrame([input_text], columns=['questions'])
     return df_input
 
 
-st.sidebar.title("Bowhead Bot")
 st.title("""
 Bowhead Bot  
 Bowhead Bot's main functionality is to help you find the information about medical trials
 """)
+st.sidebar.title("Bowhead Bot")
 st.sidebar.write('Please enter you name')
 user_name = st.sidebar.text_input('You', key=1)
-# st.sidebar.write('Please enter your city')
-# user_location = st.sidebar.text_input('You', key=2)
 user_location = st.sidebar.selectbox('Please select you city', world_cities_df)
 user_location = str(user_location)
 user_age = st.sidebar.slider('Please select you age', 0, 100, 50, 1)
 user_age = str(user_age) + ' ' + 'years'
 user_gender = st.sidebar.radio('Please select your gender', ('Male', 'Female', 'Other'))
 
-if st.button('Voice'):
-    import speech_recognition as sr
-    r = sr.Recognizer()
-    mic = sr.Microphone()
-    with mic as source:
-        audio = r.listen(source)
-        r.recognize_google(audio)
-
 # st.image(center, width=700)
 # st.sidebar.image(federer_image)
 # st.sidebar.image(nadal, width=350)
 user_input = get_text()
-response = botResponse(user_input, user_name, user_location, user_age, user_gender)
+if user_name and user_location and user_age:
+    response = botResponse(user_input, user_name, user_location, user_age, user_gender)
 
 if not user_name or not user_location or not user_age:
-    response = 'Hello user, Please enter your name and location on the left panel before proceeding'
+    response = 'Hello user, Please fill the details on the left panel before proceeding'
 
 if response:
     st.text_area("Bot:", value=response, height=200, max_chars=None, key=None)
-
-
-
+    # If audio is enabled, uncomment the below code, else comment it out
+    # if voice_input_check:
+    #     speech = gTTS(text=response, lang='en-US', slow=False)
+    #     speech.save("text.mp3")
+    #     playsound("text.mp3")
+    #     os.remove("text.mp3")
