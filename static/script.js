d10eba3b6a413b481b0e794342f688869f89747e
akhil-bowhead <65965856+akhil-bowhead@users.noreply.github.com>
2020-12-17 18:13:20 -0500

    minor issue

--- static/script.js
+++ static/script.js
@@ -64,49 +64,66 @@ $('#initialize_button').click(function(){
 
 $('#send_button').click(function(){
     var userText = $('#u_input').val();
-    $('#chatbox').append('<div class="alert alert-primary new-cells" role="alert">' + "<b>User: </b>" + userText + '</div>');
-    var element = $('#chatbox div.new-cells:last')[0];
-    console.log(element);
-    element.scrollIntoView();
-    console.log('success')
-    $.post('/process',
-    {
-    user_input: userText,
-  },
-    function(data,status){
-        console.log(typeof data)
-        data_object =  JSON.parse(data);
-        var botText = data_object["bot_response"];
-        $('#chatbox').append('<div class="alert alert-dark new-cells" role="alert">' + "<b>Bowhead Bot: </b>" + botText + '</div>');
-        var element = $('#chatbox div.new-cells:last')[0];
-        console.log(element);
-        element.scrollIntoView();
-        });
+    if (userText == ''){
+     $('#chatbox').append('<div class="alert alert-dark new-cells" role="alert">' + "<b>Bowhead Bot: </b>" + "Please input query" + '</div>');
+     var element = $('#chatbox div.new-cells:last')[0];
+     console.log(element);
+     element.scrollIntoView();
+     console.log('success')
+    }
+    else {
+            $('#chatbox').append('<div class="alert alert-primary new-cells" role="alert">' + "<b>User: </b>" + userText + '</div>');
+            var element = $('#chatbox div.new-cells:last')[0];
+            console.log(element);
+            element.scrollIntoView();
+            console.log('success')
+            $.post('/process',
+            {
+            user_input: userText,
+          },
+            function(data,status){
+                console.log(typeof data)
+                data_object =  JSON.parse(data);
+                var botText = data_object["bot_response"];
+                $('#chatbox').append('<div class="alert alert-dark new-cells" role="alert">' + "<b>Bowhead Bot: </b>" + botText + '</div>');
+                var element = $('#chatbox div.new-cells:last')[0];
+                console.log(element);
+                element.scrollIntoView();
+                });
+   }
     $("#u_input").val("");
-
            });
 
 $("#u_input").keypress(function(e) {
   if (e.which == 13) {
     var userText = $('#u_input').val();
-    $('#chatbox').append('<div class="alert alert-primary new-cells" role="alert">' + "<b>User: </b>" + userText + '</div>');
-    var element = $('#chatbox div.new-cells:last')[0];
-    console.log(element);
-    element.scrollIntoView();
-    console.log('success')
-    $.post('/process',
-    {
-    user_input: userText,
-  },
-    function(data,status){
-        console.log(typeof data)
-        data_object =  JSON.parse(data);
-        var botText = data_object["bot_response"];
-        $('#chatbox').append('<div class="alert alert-dark new-cells" role="alert">' + "<b>Bowhead Bot: </b>" + botText + '</div>');
-        var element = $('#chatbox div.new-cells:last')[0];
-        console.log(element);
-        element.scrollIntoView();
-        });
+    if (userText == ''){
+     $('#chatbox').append('<div class="alert alert-dark new-cells" role="alert">' + "<b>Bowhead Bot: </b>" + "Please input query" + '</div>');
+     var element = $('#chatbox div.new-cells:last')[0];
+     console.log(element);
+     element.scrollIntoView();
+     console.log('success')
+    }
+    else {
+            $('#chatbox').append('<div class="alert alert-primary new-cells" role="alert">' + "<b>User: </b>" + userText + '</div>');
+            var element = $('#chatbox div.new-cells:last')[0];
+            console.log(element);
+            element.scrollIntoView();
+            console.log('success')
+            $.post('/process',
+            {
+            user_input: userText,
+          },
+            function(data,status){
+                console.log(typeof data)
+                data_object =  JSON.parse(data);
+                var botText = data_object["bot_response"];
+                $('#chatbox').append('<div class="alert alert-dark new-cells" role="alert">' + "<b>Bowhead Bot: </b>" + botText + '</div>');
+                var element = $('#chatbox div.new-cells:last')[0];
+                console.log(element);
+                element.scrollIntoView();
+                });
+     }
 
     $(this).val("");
   }
